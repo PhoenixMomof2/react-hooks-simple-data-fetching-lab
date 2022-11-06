923213f9ac1466cf0f73bf841559add4cfba0c42
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-06 17:57:24 -0500

    commit tree 8bcb21212f2601785f02d8aa5a7eb1314bc8d789

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const baseUrl = "https://dog.ceo/api/breeds/image/random";
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+  if(!message)return <p>Loading...</p>
+
+  return (
+    <div>        
+      <img src={message} alt="A Random Dog" />
+    </div>
+  )
+};
+
+export default App;
